43d573aa8841637565a3859567d8d894750e327e
mohsenkamini <mohsenkamini@gmail.com>
2024-11-28 19:26:25 +0330

    okay with user authentication

--- tests/auth-curl.sh
+++ tests/auth-curl.sh
@@ -1,11 +1,11 @@
 curl -X POST http://localhost:8000/auth/users/ \
   -H "Content-Type: application/json" \
-  -d '{"username": "testuser", "password": "password123"}'
+  -d '{"username": "testuser", "password": "V4q57giibABXzx2S"}'
 
 
 curl -X POST http://localhost:8000/auth/jwt/create/ \
   -H "Content-Type: application/json" \
-  -d '{"username": "testuser", "password": "password123"}'
+  -d '{"username": "testuser", "password": "V4q57giibABXzx2S"}'
 
 curl -X POST http://localhost:8000/auth/jwt/refresh/ \
   -H "Content-Type: application/json" \
